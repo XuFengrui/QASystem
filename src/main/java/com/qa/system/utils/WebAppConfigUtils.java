c94415c94a86b27cfd8cd793ae3a3eea93ea1292
XuFengrui <2609231098@qq.com>
2020-04-23 22:07:21 +0800

    test10

---
+++ src/main/java/com/qa/system/utils/WebAppConfigUtils.java
@@ -0,0 +1,21 @@
+package com.qa.system.utils;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @ClassName WebAppConfigUtils
+ * @Description TODO
+ * @Author XuFengrui
+ * @Date 2020/4/23
+ * @Version 1.0
+ **/
+@Configuration
+public class WebAppConfigUtils extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("D:/icon/**").addResourceLocations("file:D:/icon");
+        super.addResourceHandlers(registry);
+    }
+}
