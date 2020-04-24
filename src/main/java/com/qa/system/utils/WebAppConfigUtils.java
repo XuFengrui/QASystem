aae862c2d80f92d6e2d5bf8df710cdc9f4bdbfae
XuFengrui <2609231098@qq.com>
2020-04-24 09:20:05 +0800

    test10

--- src/main/java/com/qa/system/utils/WebAppConfigUtils.java
+++ src/main/java/com/qa/system/utils/WebAppConfigUtils.java
@@ -1,8 +1,9 @@
 package com.qa.system.utils;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @ClassName WebAppConfigUtils
@@ -12,9 +13,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * @Version 1.0
  **/
 @Configuration
-public class WebAppConfigUtils extends WebMvcConfigurerAdapter {
+public class WebAppConfigUtils implements WebMvcConfigurer {
+
+    @Value("${uploadFile.path}")
+    private String uploadFilePath;
+
+    @Value("${uploadFile.staticAccessPath}")
+    private String staticAccessPath;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("D:/icon/**").addResourceLocations("file:D:/icon/");
+        registry.addResourceHandler(staticAccessPath).addResourceLocations("file:"+uploadFilePath);
     }
 }
